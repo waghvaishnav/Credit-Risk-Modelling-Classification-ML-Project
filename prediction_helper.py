5864bc8bd229972168fcf1d4d7390c71b61cb49a
waghvaishnav <waghvaishnav4@gmail.com>
2026-02-16 16:08:39 +0530

    path change

--- prediction_helper.py
+++ prediction_helper.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 # Path to the saved model and its components
-MODEL_PATH = "C:/Users/Vaishnav/OneDrive/Documents/Credit Risk Modelling/artifacts/model_data.joblib"
+MODEL_PATH = r"C:/Users/Vaishnav/OneDrive/Documents/Credit Risk Modelling/artifacts/model_data.joblib"
 
 # Load the model and its components
 model_data = joblib.load(MODEL_PATH)
